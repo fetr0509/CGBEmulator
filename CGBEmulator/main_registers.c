25c4d97823159008db33357e5d34e2aafff0c385
Peter Fetros <fetr0509@vandals.uidaho.edu>
2017-12-21 22:47:45 -0800

    Added Skeleton of the main registers used by the gameboy as well as generic functions to set 8 and 16 bit registers.

--- CGBEmulator/main_registers.c
+++ CGBEmulator/main_registers.c
@@ -8,3 +8,24 @@
 
 #include "main_registers.h"
 
+void setRegA(mainRegisters regs, uint8_t value) {regs.reg_A = value;}
+void setRegF(mainRegisters regs, uint8_t value) {regs.reg_F = value;}
+void setRegB(mainRegisters regs, uint8_t value) {regs.reg_B = value;}
+void setRegC(mainRegisters regs, uint8_t value) {regs.reg_C = value;}
+void setRegD(mainRegisters regs, uint8_t value) {regs.reg_D = value;}
+void setRegE(mainRegisters regs, uint8_t value) {regs.reg_E = value;}
+void setRegH(mainRegisters regs, uint8_t value) {regs.reg_H = value;}
+void setRegL(mainRegisters regs, uint8_t value) {regs.reg_L = value;}
+
+void setRegBC(mainRegisters regs, uint16_t value) {
+    regs.reg_B = value >> 8;
+    regs.reg_C = value & LOWERBITS;
+}
+void setRegDE(mainRegisters regs, uint16_t value) {
+    regs.reg_D = value >> 8;
+    regs.reg_E = value & LOWERBITS;
+}
+void setRegHL(mainRegisters regs, uint16_t value) {
+    regs.reg_H = value >> 8;
+    regs.reg_L = value & LOWERBITS;
+}
--- CGBEmulator/main_registers.h
+++ CGBEmulator/main_registers.h
@@ -11,5 +11,43 @@
 
 #include <stdio.h>
 
+#define zeroFlag(flagReg) ((flagReg >> 7) & 1)
+#define subtractFlag(flagReg) ((flagReg >> 6) & 1)
+#define halfCarryFlag(flagReg) ((flagReg >> 5) & 1)
+#define carryFlag(flagReg) ((flagReg >> 4) & 1)
+
+#define LOWERBITS (0x0F)
+
+
+typedef struct mainRegisters {
+    uint8_t reg_A;
+    uint8_t reg_F;
+    
+    uint8_t reg_B;
+    uint8_t reg_C;
+    
+    uint8_t reg_D;
+    uint8_t reg_E;
+    
+    uint8_t reg_H;
+    uint8_t reg_L;
+    
+    uint16_t programCounter;
+    uint16_t stackPointer;
+} mainRegisters;
+
+void setRegA(mainRegisters regs, uint8_t value);
+void setRegF(mainRegisters regs, uint8_t value);
+void setRegB(mainRegisters regs, uint8_t value);
+void setRegC(mainRegisters regs, uint8_t value);
+void setRegD(mainRegisters regs, uint8_t value);
+void setRegE(mainRegisters regs, uint8_t value);
+void setRegH(mainRegisters regs, uint8_t value);
+void setRegL(mainRegisters regs, uint8_t value);
+
+void setRegAF(mainRegisters regs, uint16_t value);
+void setRegBC(mainRegisters regs, uint16_t value);
+void setRegDE(mainRegisters regs, uint16_t value);
+void setRegHL(mainRegisters regs, uint16_t value);
 
 #endif /* main_registers_h */
