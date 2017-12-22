00f9c9af1080569f6a9daf6ed593931eb9b86fe0
Peter Fetros <fetr0509@vandals.uidaho.edu>
2017-12-21 21:44:47 -0800

    Initial Commit

---
+++ CGBEmulator/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CGBEmulator
+//
+//  Created by Peter Fetros on 12/21/17.
+//  Copyright © 2017 Peter Fetros. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
